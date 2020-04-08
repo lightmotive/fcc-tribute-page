1cd4f89795a99bde785f496939cc793be95823eb
alight1 <andrelight.al@gmail.com>
2020-04-08 15:22:20 -0600

    Update Webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,22 +1,14 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: "assets"
+        path: path.resolve(__dirname, 'dist')
     },
-    devServer: {
-        contentBase: './dist',
-    },
-    devtool: 'inline-source-map',
     plugins: [
-        // new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: "Jóhann Jóhannsson Tribute",
             template: './src/index.html'
         })
     ],
@@ -38,18 +30,15 @@ module.exports = {
                 test: /\.(jpe?g|png|gif)$/,
                 loader: 'url-loader',
                 options: {
-                    // Images larger than 10 KB won’t be inlined
-                    limit: 10 * 1024,
-                    publicPath: 'assets'
+                    // Images larger than 1 KB won’t be inlined
+                    limit: 1024
                 }
             },
             {
                 test: /\.svg$/i,
                 use: {
                     loader: 'svg-url-loader',
-                    options: {
-                        publicPath: 'assets'
-                    }
+                    options: {}
                 }
             }
         ],
