30c982b5aad04099e3c94d0910e6766783ee82eb
alight1 <andrelight.al@gmail.com>
2020-04-14 13:33:36 -0600

    Update external script loading
    * Load jquery from Google CDN (add to externals) via index.html
    * Load freeCodeCamp testing utility from index.html

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
-require('../src/style-common.css');
-require('../src/style.css');
-require('readmore-js');
-import $ from 'jquery';
-import assetChevronDown from '../src/assets/font-awesome/chevron-down.svg';
-import assetChevronUp from '../src/assets/font-awesome/chevron-up.svg';
-import bodyContent from '../src/body-content.html'
+require("../src/style-common.css");
+require("../src/style.css");
+require("readmore-js");
+import $ from "jquery";
+import assetChevronDown from "../src/assets/font-awesome/chevron-down.svg";
+import assetChevronUp from "../src/assets/font-awesome/chevron-up.svg";
+import bodyContent from "../src/body-content.html";
 
 document.getElementById("body").innerHTML += bodyContent;
 
-$('.event-description').readmore({
-    moreLink: '<a href="#"><img src="' + assetChevronDown + '" class="read-more-open" alt="Read more" /></a>',
-    lessLink: '<a href="#"><img src="' + assetChevronUp + '" class="read-more-close" alt="Close" /></a>',
-    heightMargin: 50
-});
+window.onload = () => {};
 
-//Add freeCodeCamp script (cannot add to dynamically loaded HTML like body-content.html)
-let fccScript = document.createElement('script');
-fccScript.src = 'https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js';
-document.head.appendChild(fccScript);
+$(".event-description").readmore({
+  moreLink:
+    '<a href="#"><img src="' +
+    assetChevronDown +
+    '" class="read-more-open" alt="Read more" /></a>',
+  lessLink:
+    '<a href="#"><img src="' +
+    assetChevronUp +
+    '" class="read-more-close" alt="Close" /></a>',
+  heightMargin: 50,
+});
--- webpack.common.js
+++ webpack.common.js
@@ -1,42 +1,45 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: '[contenthash].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
+  entry: "./src/index.js",
+  output: {
+    filename: "[contenthash].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  externals: {
+    jquery: "jQuery",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        loader: "url-loader",
+        options: {
+          // Images larger than 1 KB won’t be inlined
+          limit: 1024,
+        },
+      },
+      {
+        test: /\.svg$/i,
+        use: {
+          loader: "svg-url-loader",
+          options: {},
+        },
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: ['html-loader']
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(jpe?g|png|gif)$/,
-                loader: 'url-loader',
-                options: {
-                    // Images larger than 1 KB won’t be inlined
-                    limit: 1024
-                }
-            },
-            {
-                test: /\.svg$/i,
-                use: {
-                    loader: 'svg-url-loader',
-                    options: {}
-                }
-            }
-        ],
-    }
+  },
 };
